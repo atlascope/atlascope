d1ca27c7963ed18623be1f5efa3c23e9867a6513
Anne Haley <anne.haley@kitware.com>
2021-11-15 18:46:10 -0500

    Make Investigations and Datasets permissions-based

--- atlascope/core/management/commands/populate.py
+++ atlascope/core/management/commands/populate.py
@@ -1,7 +1,9 @@
 import json
 
 from django.contrib.auth.models import User
+from guardian.shortcuts import assign_perm
 import djclick as click
+from oauth2_provider.models import Application
 
 from atlascope.core.models import Investigation
 
@@ -14,9 +16,12 @@ MODEL_JSON_MAPPING = [
 
 DEFAULT_PASSWORD = '123'
 
+DEFAULT_CLIENT_URI = 'http://localhost:8081/'
+
 
 def expand_references(obj, model):
     many_to_many_values = {}
+    permissions = {}
     for field_name, value in obj.items():
         found_field = [field for field in model._meta.fields if field.name == field_name]
         found_field = found_field[0] if len(found_field) > 0 else None
@@ -33,12 +38,28 @@ def expand_references(obj, model):
                 ]
     for field_name in many_to_many_values.keys():
         del obj[field_name]
-    return obj, many_to_many_values
+    for field_name, permission in {
+        'investigators': 'change_investigation',
+        'observers': 'view_investigation',
+    }.items():
+        if field_name in obj:
+            permissions[permission] = [
+                User.objects.get(username=username) for username in obj[field_name]
+            ]
+            del obj[field_name]
+    return obj, many_to_many_values, permissions
 
 
 @click.option('--password', type=click.STRING, help='password to apply to all users')
 @click.command()
 def command(password):
+    Application.objects.get_or_create(
+        client_id='cBmD6D6F2YAmMWHNQZFPUr4OpaXVpW5w4Thod6Kj',
+        client_type='public',
+        redirect_uris=DEFAULT_CLIENT_URI,
+        authorization_grant_type='authorization-code',
+        skip_authorization=True,
+    )
     # delete in reverse order because of dependency protections
     for model, _ in reversed(MODEL_JSON_MAPPING):
         model.objects.all().delete()
@@ -47,12 +68,14 @@ def command(password):
         print('-----')
         objects = json.load(open(DATALOADER_DIR + filename))
         for obj in objects:
-            obj, many_to_many_values = expand_references(obj, model)
+            obj, many_to_many_values, permissions = expand_references(obj, model)
             db_obj = model(**obj)
             db_obj.save()
             print(f'Saved {model.__name__}: {list(obj.values())[0]}')
             for field_name, relations in many_to_many_values.items():
                 getattr(db_obj, field_name).set(relations)
+            for perm, user_list in permissions.items():
+                [assign_perm(perm, user, db_obj) for user in user_list]
             if model == User:
                 db_obj.set_password(password or DEFAULT_PASSWORD)
                 print(f'Password set to "{password or DEFAULT_PASSWORD}".')
