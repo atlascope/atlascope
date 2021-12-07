23bb097dafb11bb1f6e1a35787537ae26ec4a788
Anne Haley <anne.haley@kitware.com>
2021-12-07 16:01:58 -0500

    Add perform import endpoint

--- atlascope/core/rest/endpoints/dataset_endpoints.py
+++ atlascope/core/rest/endpoints/dataset_endpoints.py
@@ -1,9 +1,13 @@
+from drf_yasg.utils import no_body, swagger_auto_schema
 from guardian.shortcuts import get_objects_for_user
-from rest_framework import mixins
+from rest_framework import mixins, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from atlascope.core.models import Dataset, DatasetSerializer
+from atlascope.core.rest.permissions import object_permission_required
 
 
 class DatasetViewSet(
@@ -24,3 +28,18 @@ class DatasetViewSet(
         public_datasets = Dataset.objects.filter(public=True)
         datasets = visible_datasets | public_datasets
         return datasets.all().order_by('name')
+
+    @swagger_auto_schema(
+        request_body=no_body,
+        responses={204: 'Import successful.'},
+    )
+    @object_permission_required(model=Dataset)
+    @action(
+        detail=True,
+        methods=['POST'],
+        url_path='import',
+    )
+    def perform_import(self, request, **kwargs):
+        dataset: Dataset = self.get_object()
+        dataset.perform_import()
+        return Response(status=status.HTTP_204_NO_CONTENT)
