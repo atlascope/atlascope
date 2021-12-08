71d42e0d38bcab52f51d4f5009dcd5a7c9709f21
Matt Covalt <matt@kiwiziti.com>
2021-12-08 17:05:16 -0500

    Add center and zoom functions

--- src/utilities/useGeoJS.ts
+++ src/utilities/useGeoJS.ts
@@ -6,6 +6,9 @@ import geo from 'geojs';
 
 export default function useGeoJS(element: Ref<HTMLElement | null>) {
   const map: Ref<any> = ref(null);
+  const zoomLevel = ref(0);
+  const xCoord = ref(0);
+  const yCoord = ref(0);
 
   onMounted(() => {
     // should always be HTMLElement in onMounted if passed a valid div
@@ -13,6 +16,22 @@ export default function useGeoJS(element: Ref<HTMLElement | null>) {
     if (element.value !== null) {
       map.value = geo.map({ node: element.value });
       map.value.createLayer('osm');
+      zoomLevel.value = map.value.zoom();
     }
   });
+
+  const center = (x: number, y: number) => {
+    if (map.value !== null) {
+      map.value.center({ x, y });
+      xCoord.value = x;
+      yCoord.value = y;
+    }
+  };
+
+  const zoom = (level: number) => {
+    if (map.value !== null) {
+      map.value.zoom(level);
+      zoomLevel.value = level;
+    }
+  };
 }
