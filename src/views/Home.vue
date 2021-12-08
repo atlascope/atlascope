6c939c1fc8e2ed3a148a6f07c3804c777ea741c0
Matt Covalt <matt@kiwiziti.com>
2021-12-08 17:06:21 -0500

    Update Home component to use useGeoJS refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <script lang="ts">
-import { defineComponent, inject } from '@vue/composition-api';
+import {
+  ref, defineComponent, inject, onMounted,
+} from '@vue/composition-api';
 import OAuthClient from '@girder/oauth-client';
 import useGeoJS from '../utilities/useGeoJS';
 
 export default defineComponent({
   setup() {
+    const map = ref(null);
+
     const oauthClient = inject<OAuthClient>('oauthClient');
     if (oauthClient === undefined) {
       throw new Error('Must provide "oauthClient" into component.');
     }
-    useGeoJS();
-    return { oauthClient };
+    const { zoom, center } = useGeoJS(map);
+
+    onMounted(() => {
+      setTimeout(() => center(-0.1704, 51.5047), 2000);
+      setTimeout(() => zoom(14), 4000);
+    });
+    return { oauthClient, map };
   },
   computed: {
     loginText(): string {
@@ -28,4 +37,20 @@ export default defineComponent({
   },
 });
 </script>
-<template />
+
+<template>
+  <div
+    ref="map"
+    class="map"
+  />
+</template>
+
+<style scoped>
+.map {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
