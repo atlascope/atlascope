16e52148ac3cd1a8767c3a97df30542ae661cb32
Matt Covalt <matt@kiwiziti.com>
2021-12-08 17:04:17 -0500

    Pass in element instead of using getCurrentInstance

--- src/utilities/useGeoJS.ts
+++ src/utilities/useGeoJS.ts
@@ -1,15 +1,18 @@
-import { ref, onMounted, getCurrentInstance } from '@vue/composition-api';
+import {
+  ref, onMounted, Ref,
+} from '@vue/composition-api';
+
 import geo from 'geojs';
 
-export default function useGeoJS() {
-  const map = ref(null);
+export default function useGeoJS(element: Ref<HTMLElement | null>) {
+  const map: Ref<any> = ref(null);
 
   onMounted(() => {
-    const inst = getCurrentInstance();
-    if (inst) {
-      const geojsmap = geo.map({ node: inst.vnode.elm });
-      geojsmap.createLayer('osm');
-      map.value = geojsmap;
+    // should always be HTMLElement in onMounted if passed a valid div
+    // TODO: handle case when passed invalid div
+    if (element.value !== null) {
+      map.value = geo.map({ node: element.value });
+      map.value.createLayer('osm');
     }
   });
 }
