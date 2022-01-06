eb04d4a21916e8af9ea1d3b48790cc7d83b13ef2
Anne Haley <anne.haley@kitware.com>
2022-01-06 16:26:34 -0500

    Use three-model Job architecture in dataloader

--- atlascope/core/tasks.py
+++ atlascope/core/tasks.py
@@ -1,54 +1,52 @@
 import imp
 import io
-
 from PIL import Image
 from celery import shared_task
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.utils import timezone
 
-from atlascope.core.models import Job
-
-
-class ImageStorageBuffer:
-    def __init__(self, image_bytes):
-        self.image_bytes = image_bytes
-
-    def read(self, length):
-        return self.image_bytes.getvalue()
+from atlascope.core.models import JobRun, JobRunOutputImage
 
 
 @shared_task
-def spawn_job(job_id):
+def spawn_job(job_run_id):
     # celery arguments must be serializable
-    job = Job.objects.get(id=job_id)
-    script = job.script_contents.read()
-    input_image = Image.open(io.BytesIO(job.input_image.read()))
-    kwargs = job.other_inputs or {}
+    job_run = JobRun.objects.get(id=job_run_id)
+    script = job_run.script.script_contents.read()
+    input_image = Image.open(io.BytesIO(job_run.input_image.read()))
+    kwargs = job_run.other_inputs or {}
 
     # TODO: sandbox this
     module = imp.new_module('main')
     exec(script, module.__dict__)
     output_array = module.main(input_image=input_image, **kwargs)
 
-    storage_client = job.input_image.storage.client
-    bucket_name = job.input_image.storage.bucket_name
-
     def store_image(image, output_index):
+        filename = f'{job_run_id}_image_output_{output_index}.png'
         image_bytes = io.BytesIO()
         image.save(image_bytes, format="PNG")
-        object_name = str(job.id) + '_output_' + str(output_index) + '.png'
-        storage_client.put_object(
-            bucket_name,
-            object_name,
-            data=ImageStorageBuffer(image_bytes),
-            length=image_bytes.getbuffer().nbytes,
-            content_type="image/*",
+        image_file = InMemoryUploadedFile(
+            image_bytes,
+            None,
+            filename,
+            'image/png',
+            image_bytes.getbuffer().nbytes,
+            None,
         )
-        return f'/{bucket_name}/{object_name}'
 
-    job.outputs = [
-        output if not isinstance(output, Image.Image) else store_image(output, index)
-        for index, output in enumerate(output_array)
-    ]
-    job.last_run = timezone.now()
-    job.save()
-    print(job.outputs)
+        image_output_obj = JobRunOutputImage(job_run=job_run)
+        image_output_obj.stored_image.save(filename, image_file)
+        image_output_obj.save()
+
+    job_run.outputs = list(
+        filter(
+            None,
+            [
+                output if not isinstance(output, Image.Image) else store_image(output, index)
+                for index, output in enumerate(output_array)
+            ],
+        )
+    )
+    job_run.last_run = timezone.now()
+    job_run.save()
