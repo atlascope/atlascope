56f1a26382056d35f485e5ad42b6dfe34b2976e8
naglepuff <mike.nagler@kitware.com>
2022-01-07 13:45:35 -0500

    Create InvestigationsList view
    Create placeholder InvestigationDetail view

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import InvestigationsList from '../views/InvestigationsList.vue';
+import InvestigationDetail from '../views/InvestigationDetail.vue';
 
 Vue.use(VueRouter);
 
@@ -8,6 +10,17 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/investigations',
+    name: 'investigationsList',
+    component: InvestigationsList,
+  },
+  {
+    path: '/investigations/:investigation',
+    name: 'investigationDetail',
+    component: InvestigationDetail,
+    props: true,
   },
 ];
 
