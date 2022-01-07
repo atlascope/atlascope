69e9f88508bb9e01672fce6d41da93e9bac9d5cf
Matt Covalt <matt@kiwiziti.com>
2022-01-07 16:47:27 -0500

    Add `TileMetadataSerializer`

--- atlascope/core/rest/additional_serializers.py
+++ atlascope/core/rest/additional_serializers.py
@@ -12,3 +12,30 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
         ]
+
+
+class TileMetadataSerializer(serializers.Serializer):
+
+    levels = serializers.IntegerField(
+        help_text='Number of zoom levels in the image.',
+        min_value=1,
+        read_only=True,
+    )
+    size_x = serializers.IntegerField(
+        help_text='Image size in the X direction.',
+        min_value=1,
+        read_only=True,
+        source='sizeX',
+    )
+    size_y = serializers.IntegerField(
+        help_text='Image size in the Y direction.',
+        min_value=1,
+        read_only=True,
+        source='sizeY',
+    )
+    tile_size = serializers.IntegerField(
+        help_text='Size of the square tiles the image is composed of.',
+        min_value=1,
+        read_only=True,
+        source='tileSize',
+    )
