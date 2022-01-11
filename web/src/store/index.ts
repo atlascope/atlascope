a709b5a01eea982f8d5c4f562cd5b3026231b2e1
naglepuff <mike.nagler@kitware.com>
2022-01-11 13:37:03 -0500

    Add toolbar and authentication
    Also make `investigationsList` the view for `/` for now.
    Fix lint issues.

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -7,54 +7,54 @@ import { User, Investigation } from '../generatedTypes/AtlascopeTypes';
 Vue.use(Vuex);
 
 export interface State {
-    userInfo: User | null,
-    investigations: Investigation[],
-};
+    userInfo: User | null;
+    investigations: Investigation[];
+}
 
 const {
-    store,
-    rootActionContext,
-    moduleActionContext,
-    rootGetterContext,
-    moduleGetterContext,
+  store,
+  rootActionContext,
+  moduleActionContext,
+  rootGetterContext,
+  moduleGetterContext,
 } = createDirectStore({
-    state: {
-        userInfo: null,
-        investigations: [],
-    } as State,
-    mutations: {
-        setInvestigations(state, investigations: Investigation[]) {
-            state.investigations = investigations;
-        },
-        setUserInfo(state, userInfo: User | null) {
-            state.userInfo = userInfo;
-        }
+  state: {
+    userInfo: null,
+    investigations: [],
+  } as State,
+  mutations: {
+    setInvestigations(state, investigations: Investigation[]) {
+      state.investigations = investigations;
     },
-    actions: {
-        async fetchInvestigations(context, axiosInstance) {
-            const { commit } = rootActionContext(context);
-            const investigations = (await axiosInstance.get('/investigations')).data;
-            commit.setInvestigations(investigations.results);
-        },
-        async fetchUserInfo(context, axiosInstance) {
-            const { commit } = rootActionContext(context);
-            const userInfo = (await axiosInstance.get('/users/me')).data;
-            commit.setUserInfo(userInfo);
-        },
-        logout(context) {
-            const { commit } = rootActionContext(context);
-            commit.setUserInfo(null);
-            commit.setInvestigations([]);
-        }
-    }
+    setUserInfo(state, userInfo: User | null) {
+      state.userInfo = userInfo;
+    },
+  },
+  actions: {
+    async fetchInvestigations(context, axiosInstance) {
+      const { commit } = rootActionContext(context);
+      const investigations = (await axiosInstance.get('/investigations')).data;
+      commit.setInvestigations(investigations.results);
+    },
+    async fetchUserInfo(context, axiosInstance) {
+      const { commit } = rootActionContext(context);
+      const userInfo = (await axiosInstance.get('/users/me')).data;
+      commit.setUserInfo(userInfo);
+    },
+    logout(context) {
+      const { commit } = rootActionContext(context);
+      commit.setUserInfo(null);
+      commit.setInvestigations([]);
+    },
+  },
 });
 
 export default store;
 export {
-    rootActionContext,
-    moduleActionContext,
-    rootGetterContext,
-    moduleGetterContext,
+  rootActionContext,
+  moduleActionContext,
+  rootGetterContext,
+  moduleGetterContext,
 };
 
 // Enable types in injected $store
@@ -63,4 +63,4 @@ declare module 'vuex' {
     interface Store<S> {
         direct: ApplicationStore;
     }
-};
+}
--- web/src/views/InvestigationsList.vue
+++ web/src/views/InvestigationsList.vue
@@ -1,82 +1,95 @@
 <template>
-    <v-container class="pa-6" fluid>
-        <v-row class="pa-6">
-            <v-col v-for="investigation in investigations" :key="investigation.id" md="3">
-                <v-card elevation="9"  rounded-lg outline>
-                    <v-card-title>{{ investigation.name }}</v-card-title>
-                    <v-card-subtitle>{{ investigation.owner }}</v-card-subtitle>
-                    <v-card-text>{{ investigation.description }}</v-card-text>
-                    <v-card-actions>
-                        <router-link :to="`/investigations/${investigation.id}`">
-                            <v-btn color="blue" text>Investigate</v-btn>
-                        </router-link>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container
+    class="pa-6"
+    fluid
+  >
+    <v-row
+      v-if="!userInfo"
+      class="pa-6"
+    >
+      <v-col md="9">
+        <v-card
+          elevation="9"
+          rounded-lg
+          outline
+        >
+          <v-card-title>Info:</v-card-title>
+          <v-card-text>Please log in to begin using Atlascope</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row
+      v-else-if="investigations.length === 0"
+      class="pa-6"
+    >
+      <v-col md="9">
+        <v-card
+          elevation="9"
+          rounded-lg
+          outline
+        >
+          <v-card-title>Info:</v-card-title>
+          <v-card-text>Could not find existing investigations.</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row
+      v-else
+      class="pa-6"
+    >
+      <v-col
+        v-for="investigation in investigations"
+        :key="investigation.id"
+        md="3"
+      >
+        <v-card
+          elevation="9"
+          rounded-lg
+          outline
+        >
+          <v-card-title>{{ investigation.name }}</v-card-title>
+          <v-card-subtitle>{{ investigation.owner }}</v-card-subtitle>
+          <v-card-text>{{ investigation.description }}</v-card-text>
+          <v-card-actions>
+            <router-link :to="`/investigations/${investigation.id}`">
+              <v-btn
+                color="blue"
+                text
+              >
+                Investigate
+              </v-btn>
+            </router-link>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue';
-import { Investigation } from '../generatedTypes/AtlascopeTypes';
+import { AxiosInstance } from 'axios';
+import Vue from 'vue';
+import { computed, inject, onMounted } from '@vue/composition-api';
+
+import store from '../store';
 
 export default Vue.extend({
-    props: {
+  setup() {
+    const axiosInstance = inject<AxiosInstance>('axios');
+    const investigations = computed(() => store.state.investigations);
+    const userInfo = computed(() => store.state.userInfo);
 
-    },
+    onMounted(async () => {
+      if (axiosInstance) {
+        await store.dispatch.fetchInvestigations(axiosInstance);
+      }
+    });
 
-    data() {
-        return {};
-    },
-
-    computed: {
-        investigations(): Array<Investigation> {
-            return [
-                {
-                    id: '1',
-                    name: 'Investigation 1',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Alice',
-                },
-                {
-                    id: '2',
-                    name: 'Investigation 2',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Bob',
-                },
-                {
-                    id: '3',
-                    name: 'Investigation 3',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Carol',
-                },
-                {
-                    id: '4',
-                    name: 'Investigation 4',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Dan',
-                },
-                {
-                    id: '5',
-                    name: 'Investigation 5',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Eileen',
-                },
-                {
-                    id: '6',
-                    name: 'Investigation 6',
-                    description: 'Lorem ipsum ...',
-                    owner: 'Frank',
-                },
-            ];
-        },
-    },
-
-    methods: {
-        printSomething() {
-            console.log('Clicked a card action');
-        },
-    },
+    return {
+      axiosInstance,
+      investigations,
+      userInfo,
+    };
+  },
 });
 </script>
