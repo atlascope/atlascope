9b0462d3322f9df2eebd866850b57a0dc83240cb
naglepuff <mike.nagler@kitware.com>
2022-01-12 15:54:14 -0500

    Create LoginBanner component

--- web/src/views/InvestigationsList.vue
+++ web/src/views/InvestigationsList.vue
@@ -3,36 +3,16 @@
     class="pa-6"
     fluid
   >
-    <v-row
-      v-if="!userInfo"
-      class="pa-6"
-    >
-      <v-col md="9">
-        <v-card
-          elevation="9"
-          rounded-lg
-          outline
-        >
-          <v-card-title>Info:</v-card-title>
-          <v-card-text>Please log in to begin using Atlascope</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row
+    <login-banner v-if="!userInfo" />
+    <v-banner
       v-else-if="investigations.length === 0"
-      class="pa-6"
+      single-line
     >
-      <v-col md="9">
-        <v-card
-          elevation="9"
-          rounded-lg
-          outline
-        >
-          <v-card-title>Info:</v-card-title>
-          <v-card-text>Could not find existing investigations.</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-icon left>
+        mdi-information
+      </v-icon>
+      Could not find any existing investigations.
+    </v-banner>
     <v-row
       v-else
       class="pa-6"
@@ -70,10 +50,15 @@
 import { AxiosInstance } from 'axios';
 import Vue from 'vue';
 import { computed, inject, onMounted } from '@vue/composition-api';
+import LoginBanner from '../components/LoginBanner.vue';
 
 import store from '../store';
 
 export default Vue.extend({
+  components: {
+    LoginBanner,
+  },
+
   setup() {
     const axiosInstance = inject<AxiosInstance>('axios');
     const investigations = computed(() => store.state.investigations);
