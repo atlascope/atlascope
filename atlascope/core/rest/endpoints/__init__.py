5cbb691cc55cf0bc2fc484833281866887214dba
Anne Haley <anne.haley@kitware.com>
2022-01-18 09:21:33 -0500

    Lint fixes

--- atlascope/core/rest/endpoints/__init__.py
+++ atlascope/core/rest/endpoints/__init__.py
@@ -1,8 +1,8 @@
 from .dataset_endpoints import DatasetViewSet
 from .investigation_endpoints import InvestigationViewSet
+from .job_endpoints import JobRunViewSet, JobScriptViewSet
 from .pin_endpoints import PinViewSet
 from .user_endpoints import UserViewSet
-from .job_endpoints import JobScriptViewSet, JobRunViewSet
 
 __all__ = [
     UserViewSet,
