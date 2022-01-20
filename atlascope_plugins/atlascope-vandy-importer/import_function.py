8e6fe2bfb5187d1c7f5033a05eb7cf592021ca80
Mary Salvi <mary.salvi@kitware.com>
2022-01-20 11:07:56 -0500

    add token generating to importer

--- atlascope_plugins/atlascope-vandy-importer/import_function.py
+++ atlascope_plugins/atlascope-vandy-importer/import_function.py
@@ -1,31 +1,36 @@
 import requests
-from process_function import vandy_processor
 
-
-# def run(source_uri=None):
-#     print(f"Congratulations! You have imported {source_uri} from vandy.")
 
 def run(source_uri=None):
 
-    headers = {'Girder-Token': ''}
+    params = {'key': ''}
+    tokenUrl = "https://styx.neurology.emory.edu/girder/api/v1/api_key/token"
 
-    r = requests.get(source_uri, headers = headers)
+    token = requests.post(tokenUrl, params=params).json()["authToken"]["token"]
 
-    data = r.json()
+    headers = {'girder-token': token}
 
+    r = requests.get(source_uri, headers=headers)
 
-    vandy_dict = {
+    if r.status_code == 200:
+        with open("test.tif", "wb") as f:
+            f.write(r.content)
 
-    "name" : data['name'],
-    "size" : data["size"],
-    "meta":{
-        "updated" : data["updated"],
-        "DSAGroupSet" : data["meta"]["DSAGroupSet"],
-        "htanMeta" : data["meta"]["htanMeta"],
-        "ioparams" : data["meta"]["ioparams"],
-        "omeSceneDescription" : data["meta"]["omeSceneDescription"],
-    }
-    }
+    # data = r.json()
+    # vandy_dict = {
+    # "name" : data['name'],
+    # "size" : data["size"],
+    # "meta":{
+    #     "updated" : data["updated"],
+    #     "DSAGroupSet" : data["meta"]["DSAGroupSet"],
+    #     "htanMeta" : data["meta"]["htanMeta"],
+    #     "ioparams" : data["meta"]["ioparams"],
+    #     "omeSceneDescription" : data["meta"]["omeSceneDescription"],
+    # }
+    # }
 # add dict to meta field on model
 # upload to s3
 # add uri to s3 field to model
+
+
+run("https://styx.neurology.emory.edu/girder/api/v1/file/60e84a1868f4fe34fc7eef72/download")
