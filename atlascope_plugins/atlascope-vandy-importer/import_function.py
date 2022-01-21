da1d952f139c78cf40ea8800433bc9602675ea3a
Mary Salvi <mary.salvi@kitware.com>
2022-01-21 16:27:47 -0500

    formatting fix

--- atlascope_plugins/atlascope-vandy-importer/import_function.py
+++ atlascope_plugins/atlascope-vandy-importer/import_function.py
@@ -5,12 +5,12 @@ import requests
 
 
 def run(source_uri=None):
-    params = {'key': os.environ.get('DJANGO_API_TOKEN')}
+    params = {"key": os.environ.get("DJANGO_API_TOKEN")}
     token_url = "https://styx.neurology.emory.edu/girder/api/v1/api_key/token"
 
     token = requests.post(token_url, params=params).json()["authToken"]["token"]
 
-    headers = {'girder-token': token}
+    headers = {"girder-token": token}
 
     r = requests.get(source_uri, headers=headers, stream=True)
 
