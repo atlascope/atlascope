0f2c18b112b0e66c8de62de02f8c271bf0b17cff
Mary Salvi <mary.salvi@kitware.com>
2022-01-21 09:46:53 -0500

    add importer for vandy dataset

--- atlascope_plugins/atlascope-vandy-importer/import_function.py
+++ atlascope_plugins/atlascope-vandy-importer/import_function.py
@@ -1,36 +1,18 @@
 import requests
+import io
+import os
 
 
 def run(source_uri=None):
-
-    params = {'key': ''}
+    params = {'key': os.environ.get('DJANGO_API_TOKEN')}
     tokenUrl = "https://styx.neurology.emory.edu/girder/api/v1/api_key/token"
 
     token = requests.post(tokenUrl, params=params).json()["authToken"]["token"]
 
     headers = {'girder-token': token}
 
-    r = requests.get(source_uri, headers=headers)
+    r = requests.get(source_uri, headers=headers, stream=True)
 
-    if r.status_code == 200:
-        with open("test.tif", "wb") as f:
-            f.write(r.content)
+    imported_file = io.BytesIO(r.content)
 
-    # data = r.json()
-    # vandy_dict = {
-    # "name" : data['name'],
-    # "size" : data["size"],
-    # "meta":{
-    #     "updated" : data["updated"],
-    #     "DSAGroupSet" : data["meta"]["DSAGroupSet"],
-    #     "htanMeta" : data["meta"]["htanMeta"],
-    #     "ioparams" : data["meta"]["ioparams"],
-    #     "omeSceneDescription" : data["meta"]["omeSceneDescription"],
-    # }
-    # }
-# add dict to meta field on model
-# upload to s3
-# add uri to s3 field to model
-
-
-run("https://styx.neurology.emory.edu/girder/api/v1/file/60e84a1868f4fe34fc7eef72/download")
+    return imported_file
