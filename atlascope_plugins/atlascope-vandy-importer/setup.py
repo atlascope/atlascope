c2b111f596673c3c8ae6d26828a682c77b897d37
Matt Covalt <matt@kiwiziti.com>
2022-01-24 12:50:02 -0500

    Move `atlascope_vandy_importer` to package
    Some versions of `setuptools` allow no package folder, but others
    don't. Giving it a package folder is the best bet.

--- atlascope_plugins/atlascope-vandy-importer/setup.py
+++ atlascope_plugins/atlascope-vandy-importer/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="atlascope-vandy-importer",
+    packages=["atlascope_vandy_importer"],
     version="0.0.1",
     entry_points={
-        "atlascope.plugins": "vandy = import_function:run",
+        "atlascope.plugins": "vandy = atlascope_vandy_importer.import_function:run",
     },
     install_requires=[
         "requests",
