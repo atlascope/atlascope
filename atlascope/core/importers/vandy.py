cd73baaec1ada29b11def6dd492d3eb8bc266839
Anne Haley <anne.haley@kitware.com>
2022-01-28 10:59:32 -0500

    VandyImporter accepts file_id and api_key

--- atlascope/core/importers/vandy.py
+++ atlascope/core/importers/vandy.py
@@ -9,8 +9,7 @@ from atlascope.core.importers import AtlascopeImporter
 class VandyImporter(AtlascopeImporter):
     def perform_import(
         self,
-        source_url: str,
-        token_url: str,
+        file_id: str,
         api_key: str = None,
     ):
         api_key = api_key or os.environ.get("DJANGO_API_TOKEN")
@@ -18,9 +17,11 @@ class VandyImporter(AtlascopeImporter):
             self.raise_schema_exception()
 
         params = {"key": api_key}
+        token_url = "https://styx.neurology.emory.edu/girder/api/v1/api_key/token"
         token = requests.post(token_url, params=params).json()["authToken"]["token"]
         headers = {"girder-token": token}
 
+        source_url = f"https://styx.neurology.emory.edu/girder/api/v1/file/{file_id}/download"
         r = requests.get(source_url, headers=headers, stream=True)
 
         # perform_import should save results to class attrs "content" and "metadata"
