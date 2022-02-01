15e5b4764687d776f89bc878799b4d2fe8b480a3
Anne Haley <anne.haley@kitware.com>
2022-01-31 21:30:12 -0500

    Add UploadImporter

---
+++ atlascope/core/importers/upload.py
@@ -0,0 +1,8 @@
+import io
+from atlascope.core.importers import AtlascopeImporter
+
+
+class UploadImporter(AtlascopeImporter):
+    def perform_import(self, content: bytes, metadata: dict = {}):
+        self.content = io.BytesIO(content)
+        self.metadata = metadata
