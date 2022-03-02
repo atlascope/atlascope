9400be626933fe93307beb3347cc399a1d58f27b
Roni Choudhury <roni.choudhury@kitware.com>
2022-03-02 18:23:29 -0500

    Promote InvestigationDetailSerializer to InvestigationSerializer

--- atlascope/core/rest/endpoints/investigation_endpoints.py
+++ atlascope/core/rest/endpoints/investigation_endpoints.py
@@ -6,7 +6,6 @@ from rest_framework.viewsets import GenericViewSet
 
 from atlascope.core.models import (
     Investigation,
-    InvestigationDetailSerializer,
     InvestigationSerializer,
     JobDetailSerializer,
     PinSerializer,
@@ -19,12 +18,7 @@ class InvestigationViewSet(
     GenericViewSet,
 ):
     queryset = Investigation.objects.all().order_by('name')
-
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return InvestigationDetailSerializer
-        else:
-            return InvestigationSerializer
+    serializer_class = InvestigationSerializer
 
     @swagger_auto_schema(responses={200: PinSerializer(many=True)})
     @action(detail=True, methods=['GET'])
