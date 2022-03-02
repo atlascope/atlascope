b5ae51c172b34e41f69ec930044c7075d2db11a2
Roni Choudhury <roni.choudhury@kitware.com>
2022-03-02 14:55:44 -0500

    Fix up tests

--- atlascope/tests/conftest.py
+++ atlascope/tests/conftest.py
@@ -10,38 +10,25 @@ from s3_file_field_client import S3FileFieldClient
 from atlascope.core.management.commands.populate import POPULATE_DIR
 from atlascope.tests import factories
 
-register(factories.UserFactory)
 register(factories.InvestigationFactory)
 register(factories.DatasetFactory)
 register(factories.PinFactory)
 register(factories.JobFactory)
 
 
-@pytest.fixture(params=[None, 'superuser', 'view_model', 'change_model'])
-def user_api_client(request, user) -> APIClient:
+@pytest.fixture()
+def user_api_client(request) -> APIClient:
     def _method(**kwargs):
         api_client = APIClient()
-        api_client.force_authenticate(user=user)
-        if request.param:
-            if request.param == 'superuser':
-                user.is_superuser = True
-                user.save()
-            elif 'investigation' in kwargs:
-                assign_perm(
-                    request.param.replace('model', 'investigation'), user, kwargs['investigation']
-                )
-            elif 'dataset' in kwargs:
-                assign_perm(request.param.replace('model', 'dataset'), user, kwargs['dataset'])
         return api_client
 
     return _method
 
 
 @pytest.fixture()
-def least_perm_api_client(request, user) -> APIClient:
+def least_perm_api_client(request) -> APIClient:
     def _method(**kwargs):
         api_client = APIClient()
-        api_client.force_authenticate(user=user)
         return api_client
 
     return _method
--- atlascope/tests/test_core_endpoints.py
+++ atlascope/tests/test_core_endpoints.py
@@ -4,62 +4,13 @@ import pytest
 
 from atlascope.core import models
 from atlascope.core.job_types import available_job_types
-from atlascope.core.rest.additional_serializers import UserSerializer
-from atlascope.core.rest.permissions import has_read_perm
-
-# ------------------------------------------------------------------
-# USER ENDPOINT TESTS
-
-
-@pytest.mark.django_db
-def test_list_users(least_perm_api_client, user, user_factory):
-    users = [user_factory() for i in range(3)] + [user]
-    users.sort(key=lambda u: u.username)
-    expected_results = [UserSerializer(u).data for u in users]
-    resp = least_perm_api_client().get('/api/v1/users')
-    assert resp.status_code == 200
-    assert resp.json()['results'] == expected_results
-    assert resp.json() == {
-        'count': len(expected_results),
-        'next': None,
-        'previous': None,
-        'results': expected_results,
-    }
-
-
-@pytest.mark.django_db
-def test_retrieve_user(least_perm_api_client, user_factory):
-    target_user = user_factory()
-    resp = least_perm_api_client().get(f'/api/v1/users/{target_user.id}')
-    assert resp.status_code == 200
-    assert resp.json() == {
-        'first_name': target_user.first_name,
-        'last_name': target_user.last_name,
-        'id': target_user.id,
-        'username': target_user.username,
-        'email': target_user.email,
-    }
-
-
-@pytest.mark.django_db
-def test_retrieve_me(least_perm_api_client, user):
-    resp = least_perm_api_client().get('/api/v1/users/me')
-    assert resp.status_code == 200
-    assert resp.json() == {
-        'first_name': user.first_name,
-        'last_name': user.last_name,
-        'id': user.id,
-        'username': user.username,
-        'email': user.email,
-    }
-
 
 # ------------------------------------------------------------------
 # INVESTIGATION ENDPOINT TESTS
 
 
 @pytest.mark.django_db
-def test_list_investigations(user_api_client, user, investigation_factory):
+def test_list_investigations(user_api_client, investigation_factory):
     investigations = [investigation_factory() for i in range(3)]
     investigations.sort(key=lambda i: i.name)
     user_api_client = user_api_client(investigation=investigations[0])
@@ -68,14 +19,9 @@ def test_list_investigations(user_api_client, user, investigation_factory):
             'id': i.id,
             'name': i.name,
             'description': i.description,
-            'owner': i.owner.username,
         }
         for i in (
             investigations
-            if user.is_superuser
-            else investigations[:1]
-            if has_read_perm(user, investigations[0])
-            else []
         )
     ]
     resp = user_api_client.get('/api/v1/investigations')
@@ -89,50 +35,24 @@ def test_list_investigations(user_api_client, user, investigation_factory):
 
 
 @pytest.mark.django_db
-def test_retrieve_investigation(user_api_client, user, user_factory, investigation_factory):
-    investigation = investigation_factory(owner=user_factory())
-    resp = user_api_client(investigation=investigation).get(
+def test_retrieve_investigation(user_api_client, investigation_factory):
+    investigation = investigation_factory()
+    resp = user_api_client().get(
         f'/api/v1/investigations/{investigation.id}'
     )
-    if has_read_perm(user, investigation):
-        assert resp.status_code == 200
-        assert resp.json() == models.InvestigationDetailSerializer(investigation).data
-    else:
-        assert resp.status_code == 404
-
-
-@pytest.mark.django_db
-def test_change_investigation_permissions(user_api_client, user, user_factory, investigation):
-    new_permissions = {
-        'owner': user.username,
-        'investigators': [
-            user_factory().username,
-            user_factory().username,
-        ],
-        'observers': [
-            user_factory().username,
-            user_factory().username,
-        ],
-    }
-    resp = user_api_client(investigation=investigation).post(
-        f'/api/v1/investigations/{investigation.id}/permissions', data=new_permissions
-    )
     assert resp.status_code == 200
-    assert resp.json() == new_permissions
+    assert resp.json() == models.InvestigationDetailSerializer(investigation).data
 
 
 @pytest.mark.django_db
-def test_get_investigation_pins(user_api_client, user, investigation, pin_factory):
+def test_get_investigation_pins(user_api_client, investigation, pin_factory):
     pin_set = [pin_factory() for i in range(5)]
     investigation.pins.set(pin_set)
     resp = user_api_client(investigation=investigation).get(
         f'/api/v1/investigations/{investigation.id}/pins'
     )
-    if has_read_perm(user, investigation):
-        assert resp.status_code == 200
-        assert resp.json() == [models.PinSerializer(pin).data for pin in pin_set]
-    else:
-        assert resp.status_code == 404
+    assert resp.status_code == 200
+    assert resp.json() == [models.PinSerializer(pin).data for pin in pin_set]
 
 
 # ------------------------------------------------------------------
@@ -140,14 +60,13 @@ def test_get_investigation_pins(user_api_client, user, investigation, pin_factor
 
 
 @pytest.mark.django_db
-def test_list_datasets(user_api_client, user, dataset_factory):
+def test_list_datasets(user_api_client, dataset_factory):
     datasets = [dataset_factory() for i in range(3)]
     datasets.sort(key=lambda d: d.name)
     user_api_client = user_api_client(dataset=datasets[0])
     expected_results = [
         models.DatasetSerializer(d).data
         for d in datasets
-        if (d.public or user.is_superuser or (has_read_perm(user, d)))
     ]
     resp = user_api_client.get('/api/v1/datasets')
 
@@ -161,24 +80,14 @@ def test_list_datasets(user_api_client, user, dataset_factory):
 
 
 @pytest.mark.django_db
-def test_retrieve_dataset(user_api_client, user, dataset_factory):
-    dataset_public = dataset_factory(public=True)
-    dataset_private = dataset_factory(public=False)
+def test_retrieve_dataset(user_api_client, dataset_factory):
+    dataset = dataset_factory()
 
-    resp_public = user_api_client(dataset=dataset_public).get(
-        f'/api/v1/datasets/{dataset_public.id}'
+    resp = user_api_client(dataset=dataset).get(
+        f'/api/v1/datasets/{dataset.id}'
     )
-    assert resp_public.status_code == 200
-    assert resp_public.data == models.DatasetSerializer(dataset_public).data
-
-    resp_private = user_api_client(dataset=dataset_private).get(
-        f'/api/v1/datasets/{dataset_private.id}'
-    )
-    if has_read_perm(user, dataset_private):
-        assert resp_private.status_code == 200
-        assert resp_private.data == models.DatasetSerializer(dataset_private).data
-    else:
-        assert resp_private.status_code == 404
+    assert resp.status_code == 200
+    assert resp.data == models.DatasetSerializer(dataset).data
 
 
 # ------------------------------------------------------------------
