31914485683f1142c70f811aa6bc38d63e1c82c6
Mary Salvi <mary.salvi@kitware.com>
2022-03-09 13:54:47 -0500

    fix conflict

--- atlascope/core/rest/endpoints/dataset_endpoints.py
+++ atlascope/core/rest/endpoints/dataset_endpoints.py
@@ -1,7 +1,6 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
