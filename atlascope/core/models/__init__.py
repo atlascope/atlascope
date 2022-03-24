974957be96e8f7b555089a689445a761e33bc743
Mary Salvi <mary.salvi@kitware.com>
2022-03-24 12:57:58 -0400

    start tour concept planning

--- atlascope/core/models/__init__.py
+++ atlascope/core/models/__init__.py
@@ -2,6 +2,7 @@ from .dataset import Dataset, DatasetCreateSerializer, DatasetSerializer, Datase
 from .dataset_embedding import DatasetEmbedding, DatasetEmbeddingSerializer
 from .investigation import Investigation, InvestigationSerializer
 from .pin import Pin, PinSerializer
+from .tour import TourSerializer, TourCreateSerializer, Tour
 
 # this must be done last to prevent circular imports via the job_types module
 from .job import Job, JobSpawnSerializer, JobDetailSerializer  # isort: skip
@@ -9,6 +10,9 @@ from .job import Job, JobSpawnSerializer, JobDetailSerializer  # isort: skip
 __all__ = [
     Investigation,
     InvestigationSerializer,
+    Tour,
+    TourSerializer,
+    TourCreateSerializer,
     DatasetEmbedding,
     DatasetEmbeddingSerializer,
     Dataset,
