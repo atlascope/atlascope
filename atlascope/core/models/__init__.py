d63f6e577037cb9d9b9006acd73544e4622af193
Mary Salvi <mary.salvi@kitware.com>
2022-03-30 13:32:40 -0400

    restructure tour and waypoint

--- atlascope/core/models/__init__.py
+++ atlascope/core/models/__init__.py
@@ -2,17 +2,17 @@ from .dataset import Dataset, DatasetCreateSerializer, DatasetSerializer, Datase
 from .dataset_embedding import DatasetEmbedding, DatasetEmbeddingSerializer
 from .investigation import Investigation, InvestigationSerializer
 from .pin import Pin, PinSerializer
-from .tour import TourSerializer, TourCreateSerializer, Tour
+from .tour import Tour
+from .waypoint import Waypoint
 
 # this must be done last to prevent circular imports via the job_types module
 from .job import Job, JobSpawnSerializer, JobDetailSerializer  # isort: skip
 
 __all__ = [
+    Tour,
+    Waypoint,
     Investigation,
     InvestigationSerializer,
-    Tour,
-    TourSerializer,
-    TourCreateSerializer,
     DatasetEmbedding,
     DatasetEmbeddingSerializer,
     Dataset,
