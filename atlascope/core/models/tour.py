296bb24d6eda9a64e9b816bf292737998fc5ddb5
Mary Salvi <mary.salvi@kitware.com>
2022-03-30 13:32:40 -0400

    restructure tour and waypoint

--- atlascope/core/models/tour.py
+++ atlascope/core/models/tour.py
@@ -1,7 +1,5 @@
 # from typing import List
 from uuid import uuid4
-from django.contrib.postgres.fields import ArrayField
-from django.contrib.gis.db import models as gisModels
 
 from django.db import models
 from rest_framework import serializers
@@ -9,25 +7,10 @@ from rest_framework import serializers
 
 class Tour(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    investigation = models.ForeignKey(
-        'Investigation',
-        on_delete=models.CASCADE,
-        related_name='tour',
-    )
-    way_points = ArrayField(
-        ArrayField(
-            gisModels.PointField(null=True),
-            null=True
-        ),
-        null=True
-    )
+    way_points = models.ManyToManyField('Waypoint', through='Investigation')
 
 
 class TourSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tour
         fields = '__all__'
-
-
-class TourCreateSerializer(serializers.Serializer):
-    way_points = serializers.ListField(child=serializers.ListField(child=serializers.IntegerField()))
