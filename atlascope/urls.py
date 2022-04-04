4bb1f43a560e056d274e0d11f666695442f142b0
Anne Haley <anne.haley@kitware.com>
2022-04-04 18:15:34 -0400

    Remove last traces of s3ff

--- atlascope/urls.py
+++ atlascope/urls.py
@@ -31,7 +31,6 @@ for model_name, viewset in [
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('api/v1/s3-upload/', include('s3_file_field.urls')),
     path('api/v1/', include(router.urls)),
     path('api/docs/redoc/', schema_view.with_ui('redoc'), name='docs-redoc'),
     path('api/docs/swagger/', schema_view.with_ui('swagger'), name='docs-swagger'),
