49aebc8075926bb5ca6cbf3e9f0133ed87de598d
Matt Covalt <matt@kiwiziti.com>
2022-04-08 14:32:06 -0400

    Move embeddings into main investigation view

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import InvestigationsList from '../views/InvestigationsList.vue';
 import InvestigationDetail from '../views/InvestigationDetail.vue';
-import EmbeddingsDetail from '../views/EmbeddingsDetail.vue';
 
 Vue.use(VueRouter);
 
@@ -16,12 +15,6 @@ const routes: Array<RouteConfig> = [
     path: '/investigations/:investigation',
     name: 'investigationDetail',
     component: InvestigationDetail,
-    props: true,
-  },
-  {
-    path: '/investigations/:investigation/embeddings',
-    name: 'embeddingsDetail',
-    component: EmbeddingsDetail,
     props: true,
   },
 ];
