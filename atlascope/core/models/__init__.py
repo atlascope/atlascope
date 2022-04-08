f9b18da2522b614cf29925b11d36534655085f0f
Mary Salvi <mary.salvi@kitware.com>
2022-04-08 12:28:29 -0400

    change through table to tour_waypoints

--- atlascope/core/models/__init__.py
+++ atlascope/core/models/__init__.py
@@ -4,12 +4,14 @@ from .investigation import Investigation, InvestigationSerializer
 from .pin import Pin, PinSerializer
 from .tour import Tour
 from .waypoint import Waypoint
+from .tour_waypoints import Tour_waypoints
 
 # this must be done last to prevent circular imports via the job_types module
 from .job import Job, JobSpawnSerializer, JobDetailSerializer  # isort: skip
 
 __all__ = [
     Tour,
+    Tour_waypoints,
     Waypoint,
     Investigation,
     InvestigationSerializer,
