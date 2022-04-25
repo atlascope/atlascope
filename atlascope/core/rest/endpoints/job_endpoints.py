2006f902ece338a0206ab16f30521dd0ede45c3f
Anne Haley <anne.haley@kitware.com>
2022-04-25 12:50:09 -0400

    Return jsonschema for job types directly from API

--- atlascope/core/rest/endpoints/job_endpoints.py
+++ atlascope/core/rest/endpoints/job_endpoints.py
@@ -51,23 +51,34 @@ class JobViewSet(
     )
     @action(detail=False, methods=['GET'])
     def types(self, request, **kwargs):
+        omit_parameters = [
+            'original_dataset_id',
+            'job_id',
+        ]
+        type_mapping = {
+            int: 'integer',
+            float: 'number',
+        }
         payload = [
             {
                 'name': key,
                 'description': module.__doc__,
-                'additional_inputs': [
-                    {
-                        "name": name,
-                        "class": param.annotation.__name__,
-                        "required": param.default == Parameter.empty,
-                    }
-                    for name, param in signature(module).parameters.items()
-                    if name
-                    not in [
-                        'original_dataset_id',
-                        'job_id',
-                    ]
-                ],
+                'schema': {
+                    "type": "object",
+                    "required": [
+                        name
+                        for name, param in signature(module).parameters.items()
+                        if param.default == Parameter.empty and name not in omit_parameters
+                    ],
+                    "properties": {
+                        name: {
+                            "type": type_mapping.get(param.annotation, "string"),
+                            "title": name.replace('_', ' ').title(),
+                        }
+                        for name, param in signature(module).parameters.items()
+                        if name not in omit_parameters
+                    },
+                },
             }
             for key, module in available_job_types.items()
         ]
