a28397135ceadf6e35825bcb295415cc5e4cec63
banesullivan <bane.sullivan@kitware.com>
2022-04-25 11:24:11 -0600

    Update test

--- atlascope/tests/test_tiling.py
+++ atlascope/tests/test_tiling.py
@@ -8,3 +8,5 @@ def test_metadata(api_client):
     populate_command()
     resp = api_client().get('/api/v1/datasets/tile_source/2000/tiles/metadata')
     assert resp.status_code == 200
+    # Make sure OMETIFF reader was used
+    assert 'frames' in resp.json()['additional_metadata']
