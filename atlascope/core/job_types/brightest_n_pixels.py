2fadfde1a6567ee9c145629a33a5e038cc09ff20
Anne Haley <anne.haley@kitware.com>
2022-04-27 11:07:27 -0400

    Add a failure case to job spawning

--- atlascope/core/job_types/brightest_n_pixels.py
+++ atlascope/core/job_types/brightest_n_pixels.py
@@ -17,43 +17,47 @@ def run(job_id: str, original_dataset_id: str, n: int):
     original_dataset = Dataset.objects.get(id=original_dataset_id)
     job = Job.objects.get(id=job_id)
 
-    input_image = Image.open(io.BytesIO(original_dataset.content.read())).convert("RGB")
-    output_image = input_image.copy()
+    try:
+        input_image = Image.open(io.BytesIO(original_dataset.content.read())).convert("RGB")
+        output_image = input_image.copy()
 
-    data = np.array(input_image)
-    data = np.apply_along_axis(lambda arr: arr[:-1], 2, data)
-    data = np.apply_along_axis(np.sum, 2, data)
-    data = np.transpose(data)
+        data = np.array(input_image)
+        data = np.apply_along_axis(lambda arr: arr[:-1], 2, data)
+        data = np.apply_along_axis(np.sum, 2, data)
+        data = np.transpose(data)
 
-    brightest = []
-    while len(brightest) < n:
-        max = np.max(data)
-        maxloc = list(zip(*np.where(data == max)))[0]
-        surrounding10 = [
-            (maxloc[0] - 5 + i, maxloc[1] - 5 + j) for i in range(10) for j in range(10)
-        ]
-        for pixel in surrounding10:
-            data[pixel[0]][pixel[1]] = 0
-        brightest.append([int(val) for val in maxloc])
+        brightest = []
+        while len(brightest) < n:
+            max = np.max(data)
+            maxloc = list(zip(*np.where(data == max)))[0]
+            surrounding10 = [
+                (maxloc[0] - 5 + i, maxloc[1] - 5 + j) for i in range(10) for j in range(10)
+            ]
+            for pixel in surrounding10:
+                data[pixel[0]][pixel[1]] = 0
+            brightest.append([int(val) for val in maxloc])
 
-    draw = ImageDraw.Draw(output_image)
-    for location in brightest:
-        bounding_box = (
-            location[0] - 5,
-            location[1] - 5,
-            location[0] + 5,
-            location[1] + 5,
+        draw = ImageDraw.Draw(output_image)
+        for location in brightest:
+            bounding_box = (
+                location[0] - 5,
+                location[1] - 5,
+                location[0] + 5,
+                location[1] + 5,
+            )
+            draw.ellipse(bounding_box, outline=(255, 0, 0), width=3)
+
+        job.resulting_datasets.add(
+            save_output_dataset(
+                original_dataset,
+                job.investigation,
+                f'Brightest {n} Pixels',
+                output_image,
+                {'pixel_locations': brightest},
+            )
         )
-        draw.ellipse(bounding_box, outline=(255, 0, 0), width=3)
-
-    job.resulting_datasets.add(
-        save_output_dataset(
-            original_dataset,
-            job.investigation,
-            f'Brightest {n} Pixels',
-            output_image,
-            {'pixel_locations': brightest},
-        )
-    )
-    job.complete = True
-    job.save()
+        job.complete = True
+        job.save()
+    except Exception as e:
+        job.failure = str(e)
+        job.save()
