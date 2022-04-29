c7387919c9f8cf58f6e563f9a4e2361a7559cb22
naglepuff <mike.nagler@kitware.com>
2022-04-29 16:15:35 -0400

    Use composable for pin feature

---
+++ web/src/utilities/composableTypes.ts
@@ -0,0 +1,37 @@
+export interface GeoPosition {
+    x: number;
+    y: number;
+    z?: number;
+}
+
+interface MouseButtons {
+    left: boolean;
+    right: boolean;
+    middle: boolean;
+}
+
+export interface MouseClickEvent {
+    data: object;
+    mouse: {
+        buttonsDown: MouseButtons;
+        page: GeoPosition;
+    };
+}
+
+export interface GeoJSLayer {
+  getType: () => string;
+  drawLayer: () => void;
+  updateLayerUrl: (url: string) => void;
+  createFeature: (featureType: string) => GeoJSFeature;
+}
+
+export interface GeoJSFeature {
+  getType: () => string;
+  data: (newData: object[]) => void;
+  getData: () => object[];
+  position: (newPosition: Function) => void;
+  style: (newStyle: object | string, newValue?: string) => void;
+  draw: () => void;
+  featureGcsToDisplay: (x: number, y: number) => GeoPosition;
+  registerClickEvent: (handler: Function) => void;
+}
