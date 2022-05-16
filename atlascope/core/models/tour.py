4ab3477f040ba7eef2b7b275ddb0453ffdf83773
Mary Salvi <mary.salvi@kitware.com>
2022-05-16 17:38:37 -0400

    add nested waypoint serializer

--- atlascope/core/models/tour.py
+++ atlascope/core/models/tour.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db import models
+from atlascope.core.models.waypoint import Waypoint
 from rest_framework import serializers
 
 
@@ -9,14 +10,22 @@ class Tour(models.Model):
     investigation = models.ForeignKey(
         'Investigation',
         on_delete=models.CASCADE,
-        related_name='tour',
+        related_name='tours',
     )
 
 
 admin.site.register(Tour)
 
 
+class WaypointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Waypoint
+        fields = '__all__'
+
+
 class TourSerializer(serializers.ModelSerializer):
+    waypoints = WaypointSerializer(many=True)
+
     class Meta:
         model = Tour
         fields = '__all__'
