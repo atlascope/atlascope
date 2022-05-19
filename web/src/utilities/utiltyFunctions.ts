2ec5d2cc3ce0051511f20067bedf3aa01116587d
naglepuff <mike.nagler@kitware.com>
2022-05-19 16:47:11 -0400

    Remove utility function in favor of metadata

--- web/src/utilities/utiltyFunctions.ts
+++ web/src/utilities/utiltyFunctions.ts
@@ -18,12 +18,3 @@ export function postGisToPoint(location: string): Point | undefined {
   }
   return undefined;
 }
-
-export function getNonTiledImage(url: string): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => resolve(image);
-    image.onerror = () => reject();
-    image.src = url;
-  });
-}
