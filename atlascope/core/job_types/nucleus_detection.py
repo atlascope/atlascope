f9e746b2ac5e09edfaec37983e931415983c782a
Anne Haley <anne.haley@kitware.com>
2022-05-19 10:06:27 -0400

    Correctly order (x,y) coordinate pairs in detect nucleus job

--- atlascope/core/job_types/nucleus_detection.py
+++ atlascope/core/job_types/nucleus_detection.py
@@ -23,10 +23,12 @@ schema = {
 def detect_nuclei(input_image):
     # Invert image
     input_image = skimage.util.invert(input_image)
+
     # segment foreground
     foreground_threshold = 150
     im_fgnd_mask = sp.ndimage.binary_fill_holes(input_image < foreground_threshold)
-    # Run adaptive multi-scale LoG filter
+
+    # run adaptive multi-scale LoG filter
     min_radius = 5
     max_radius = 15
 
@@ -39,20 +41,22 @@ def detect_nuclei(input_image):
 
     # detect and segment nuclei using local maximum clustering
     local_max_search_radius = 5
+
     im_nuclei_seg_mask, seeds, maxima = htk.segmentation.nuclear.max_clustering(
         im_log_max, im_fgnd_mask, local_max_search_radius
     )
 
     # filter out small objects
     min_nucleus_area = 80
+
     im_nuclei_seg_mask = htk.segmentation.label.area_open(
         im_nuclei_seg_mask, min_nucleus_area
     ).astype(int)
 
     # return a list of polygons outlining the identified nuclei
-    # each polygon is a list of n points of the form [x, y]
+    # each polygon is a list of n points of the form [r, c]
     return [
-        skimage.measure.find_contours(im_nuclei_seg_mask == index, 0.5)[0].tolist()
+        skimage.measure.find_contours(im_nuclei_seg_mask == index, 0.5)[0]
         for index in range(1, im_nuclei_seg_mask.max())
     ]
 
@@ -63,10 +67,6 @@ def run(job_id: str, original_dataset_id: str):
 
     original_dataset = Dataset.objects.get(id=original_dataset_id)
     job = Job.objects.get(id=job_id)
-
-    from datetime import datetime
-
-    print('start', datetime.now())
 
     try:
         input_image = skimage.io.imread(
@@ -82,14 +82,17 @@ def run(job_id: str, original_dataset_id: str):
                 None,
                 {
                     'num_nuclei': len(nuclei),
-                    'nucleus_detections': nuclei,
+                    'nucleus_detections': [
+                        # switch coordinate order to get (x, y) format
+                        [[point[1], point[0]] for point in poly_points]
+                        for poly_points in nuclei
+                    ],
                 },
                 dataset_type='nucleus_detection',
             )
         )
         job.complete = True
         job.save()
-        print('end', datetime.now())
     except Exception as e:
         print('FAILURE!')
         print(e)
