47ad3b4a4fab56982685871246d3cfd90c2c7015
naglepuff <mike.nagler@kitware.com>
2022-06-06 12:27:26 -0400

    Move description and update serializer

--- atlascope/core/models/pin.py
+++ atlascope/core/models/pin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.gis.db.models import PointField
 from django.db import models
 from django.db.models import CheckConstraint, F, Q
+from drf_yasg import openapi
 from rest_framework import serializers
 
 PIN_COLORS = [
@@ -29,7 +30,6 @@ class Pin(models.Model):
     color = models.CharField(
         max_length=15, choices=PIN_COLORS, default='red', null=False, blank=False
     )
-    description = models.TextField(max_length=1000, blank=True)
     minimum_zoom = models.PositiveIntegerField(default=0)
     maximum_zoom = models.PositiveIntegerField(default=40)
 
@@ -47,6 +47,7 @@ class NotePin(Pin):
 
 
 class DatasetPin(Pin):
+    description = models.TextField(max_length=1000, blank=True)
     child = models.ForeignKey(
         'Dataset',
         on_delete=models.CASCADE,
@@ -72,12 +73,16 @@ class PinSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         try:
             notepin = instance.notepin
-            return NotePinSerializer(notepin).data
+            data = NotePinSerializer(notepin).data
+            data['pin_type'] = NotePin().__class__.__name__
+            return data
         except:
             pass
         try:
             datasetpin = instance.datasetpin
-            return DatasetPinSerializer(datasetpin).data
+            data = DatasetPinSerializer(datasetpin).data
+            data['pin_type'] = DatasetPin().__class__.__name__
+            return data
         except:
             pass
         return super().to_representation(instance)
@@ -85,6 +90,66 @@ class PinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pin
         fields = '__all__'
+        swagger_schema_fields = {
+        'type': openapi.TYPE_OBJECT,
+        'title': 'Pin',
+        'required': [
+            'id',
+            'investigation',
+            'parent',
+            'minimum_zoom',
+            'maximum_zoom',
+            'pin_type',
+            'location',
+            'color',
+        ],
+        'properties': {
+            'id': openapi.Schema(
+                title='id',
+                type=openapi.TYPE_INTEGER
+            ),
+            'investigation': openapi.Schema(
+                title='investigation',
+                type=openapi.TYPE_INTEGER
+            ),
+            'parent': openapi.Schema(
+                title='parent',
+                type=openapi.TYPE_INTEGER
+            ),
+            'minimum_zoom': openapi.Schema(
+                title='minimum_zoom',
+                type=openapi.TYPE_INTEGER
+            ),
+            'maximum_zoom': openapi.Schema(
+                title='maximum_zoom',
+                type=openapi.TYPE_INTEGER
+            ),
+            'location': openapi.Schema(
+                title='location',
+                type=openapi.TYPE_STRING
+            ),
+            'color': openapi.Schema(
+                title='color',
+                type=openapi.TYPE_STRING
+            ),
+            'pin_type': openapi.Schema(
+                title='pin_type',
+                type=openapi.TYPE_STRING
+            ),
+            'description': openapi.Schema(
+                title='description',
+                type=openapi.TYPE_STRING
+            ),
+            'note': openapi.Schema(
+                title='note',
+                type=openapi.TYPE_STRING
+            ),
+            'child': openapi.Schema(
+                title='child',
+                type=openapi.TYPE_INTEGER
+            )
+        }
+    }
 
 
 class InvestigationPinSerializer(serializers.BaseSerializer):
@@ -94,4 +159,4 @@ class InvestigationPinSerializer(serializers.BaseSerializer):
 
 @admin.register(Pin)
 class PinAdmin(admin.ModelAdmin):
-    list_display = ('id', 'color', 'description')
+    list_display = ('id', 'color')
