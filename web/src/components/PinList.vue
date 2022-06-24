47c716a0f243e0f801ada42574003d29314078df
naglepuff <mike.nagler@kitware.com>
2022-06-24 11:30:20 -0400

    Add checkbox to select/deselect all pins at once

--- web/src/components/PinList.vue
+++ web/src/components/PinList.vue
@@ -1,38 +1,53 @@
 <template>
-  <v-list
-    three-line
-    dense
-    flat
-    class="ma-0 pa-0"
-  >
-    <v-list-item-group
-      v-model="selectedPins"
-      multiple
-      @change="selectionChanged"
+  <div>
+    <v-toolbar
+      color="white"
+      flat
     >
-      <v-list-item
-        v-for="pin in pins"
-        :key="pin.id"
-        :value="pin"
-        :disabled="pin.parent !== rootDataset.id"
+      <v-checkbox
+        :input-value="selectedPins.length === pins.length"
+        :indeterminate="selectedPins.length > 0 && selectedPins.length < pins.length"
+        @change="toggleDisplayAll"
+      />
+      <v-toolbar-title>
+        Investigation Pins
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-list
+      three-line
+      dense
+      flat
+      class="ma-0 pa-0"
+    >
+      <v-list-item-group
+        v-model="selectedPins"
+        multiple
+        @change="selectionChanged"
       >
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="active"
-              :disabled="pin.parent !== rootDataset.id"
-            />
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ pinDisplayTitle(pin) }}</v-list-item-title>
-            <v-list-item-subtitle v-if="pin.pin_type === 'DatasetPin'">
-              {{ pin.description }}
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
+        <v-list-item
+          v-for="pin in pins"
+          :key="pin.id"
+          :value="pin"
+          :disabled="pin.parent !== rootDataset.id"
+        >
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                :disabled="pin.parent !== rootDataset.id"
+              />
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ pinDisplayTitle(pin) }}</v-list-item-title>
+              <v-list-item-subtitle v-if="pin.pin_type === 'DatasetPin'">
+                {{ pin.description }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script lang="ts">
@@ -47,8 +62,19 @@ export default defineComponent({
     const pins: Ref<Pin[]> = computed(() => store.state.currentPins);
     const rootDataset = computed(() => store.state.rootDataset);
     const selectedPins: Ref<Pin[]> = ref([]);
+
     function selectionChanged(pinList: Pin[]) {
       store.dispatch.updateSelectedPins(pinList);
+    }
+
+    function toggleDisplayAll() {
+      if (selectedPins.value.length === pins.value.length) {
+        selectedPins.value = [];
+        selectionChanged(selectedPins.value);
+      } else {
+        selectedPins.value = pins.value;
+        selectionChanged(selectedPins.value);
+      }
     }
 
     function pinDisplayTitle(pin: Pin) {
@@ -72,6 +98,7 @@ export default defineComponent({
       selectionChanged,
       selectedPins,
       pinDisplayTitle,
+      toggleDisplayAll,
     };
   },
 });
