cf6e3061f3a059954f507eefae009d57260c8f79
Mary Salvi <mary.salvi@kitware.com>
2022-07-11 16:40:34 -0400

    change dataTable to stepper

--- web/src/components/TourList.vue
+++ web/src/components/TourList.vue
@@ -1,36 +1,68 @@
 <template>
-<v-data-table
-  v-model="selected"
-  :headers="headers"
-  :items="tours"
-  :expanded.sync="expanded"
-  show-select
-  single-select
-  show-expand
-  item-key="id"
->
- <template
+  <v-data-table
+    v-model="expanded"
+    :headers="headers"
+    :items="tours"
+    :expanded.sync="expanded"
+    single-expand
+    show-expand
+    item-key="id"
+  >
+    <template
       v-slot:expanded-item="{ headers, item }"
     >
-      <td :colspan="headers.length" class="white" >
-         <v-data-table
-          v-model="selectedWaypoint"
-          :headers="waypointHeaders"
-          :items="item.waypoints"
-          show-select
-          single-select
-          hide-default-footer
-          item-key="id"
-        />
+      <td
+        :colspan="headers.length"
+        class="white"
+      >
+      <v-stepper
+        v-model="currentStep"
+        vertical
+      >
+       <template >
+          <div class="d-flex justify-space-between mx-5">
+            <v-btn
+              color="primary"
+              :disabled="currentStep == 0 || currentStep == 1 "
+              @click="previousStep()"
+            >
+              Previous
+            </v-btn>
+            <v-btn
+              color="primary"
+              :disabled="currentStep==item.waypoints.length"
+              @click="nextStep()"
+            >
+              Next
+            </v-btn>
+          </div>
+        </template>
+        <template v-for="n in item.waypoints.length">
+
+          <v-stepper-step
+            :key="`${n}-item.waypoints.id`"
+            :step="n"
+            editable
+            >
+            Waypoint Location: {{ item.waypoints[n-1].location.split(";")[1]}}
+
+
+            </v-stepper-step>
+
+        </template>
+
+
+
+      </v-stepper>
       </td>
-  </template>
-
-</v-data-table>
-
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts">
-import { defineComponent, Ref, computed, ref} from '@vue/composition-api';
+import {
+  defineComponent, Ref, computed, ref, watch
+} from '@vue/composition-api';
 import type { DataTableHeader } from 'vuetify';
 
 import { Tour, Waypoint } from '@/generatedTypes/AtlascopeTypes';
@@ -39,44 +71,38 @@ import store from '@/store';
 export default defineComponent({
   setup() {
     const tours: Ref<Tour[]> = computed(() => store.state.tours);
-    const selected:Ref<Tour[]> = computed({
+    const expanded: Ref<Tour[]> = computed({
       get: () => store.state.selectedTour,
       set: (val) => store.commit.setSelectedTour(val),
-    })
+    });
     const selectedWaypoint: Ref<Waypoint[]> = computed({
       get: () => store.state.selectedWaypoint,
       set: (val) => store.commit.setSelectedWaypoint(val),
-    })
-    const expanded: Tour[] = [];
-
+    });
 
     const headers: DataTableHeader[] = [
-      { text: '', value: 'data-table-select' },
       { text: 'Tour Name', value: 'name' },
       { text: '', value: 'data-table-expand' },
     ];
 
-    const waypointHeaders: DataTableHeader[] = [
-      { text: '', value: 'data-table-select' },
-      { text: 'Waypoint Location', value: 'location' },
-      { text: 'Zoom Level', value: 'zoom' },
-    ];
+    const currentStep = ref<number>(0)
 
-    // const zoomTo = (input: {item:Waypoint, value:boolean}) => {
-    //   console.log(input.value)
-    //   console.log(input.item.location)
-    //   if(input.value){
-    //     store.commit.setSelectedWaypoint(input.item);
-    //   }
-    // }
+    const nextStep = () => {
+      currentStep.value=currentStep.value+1;
+    }
+
+    const previousStep = () => {
+      currentStep.value=currentStep.value-1;
+    }
 
     return {
       tours,
-      selected,
       selectedWaypoint,
       headers,
-      waypointHeaders,
       expanded,
+      currentStep,
+      nextStep,
+      previousStep
     };
   },
 });
