02218c0fa4294619f8259f62622e3042df336d8b
Anne Haley <anne.haley@kitware.com>
2022-07-19 08:10:42 -0400

    Use nucleus detection and gland detection jobs as prerequisite information for avg_dist_to_gland job

--- atlascope/core/job_types/nucleus_to_gland_distance.py
+++ atlascope/core/job_types/nucleus_to_gland_distance.py
@@ -1,14 +1,12 @@
 """Return the average distance from nucleus centroid to nearest gland centroid."""
 
-import io
 import math
-import numpy as np
 from celery import shared_task
-import skimage.io
 
 from atlascope.core.models import Dataset
 
-from .nucleus_detection import detect_nuclei
+from .nucleus_detection import run as run_detect_nuclei
+from .gland_detection import run as run_detect_glands
 from .utils import save_output_dataset
 
 schema = {
@@ -33,6 +31,7 @@ def run(job_id: str, original_dataset_id: str, gland_map_path: str):
 
     try:
         # If a previous job has nucleus detections, use that information
+        # Else run a new job for that information
         nucleus_detections = Job.objects.filter(
             original_dataset=original_dataset,
             job_type='nucleus_detection',
@@ -40,32 +39,54 @@ def run(job_id: str, original_dataset_id: str, gland_map_path: str):
         )
         if nucleus_detections.count() > 0:
             resulting_dataset = nucleus_detections[0].resulting_datasets.first()
-            nucleus_detections = list(resulting_dataset.detected_nuclei.all())
-            nucleus_mask = np.array(resulting_dataset.metadata['nucleus_mask'])
-            nucleus_centroids = [
-                (nucleus.centroid.x, nucleus.centroid.y) for nucleus in nucleus_detections
-            ]
         else:
-            input_image = skimage.io.imread(
-                io.BytesIO(original_dataset.content.read()),
+            new_job = Job.objects.create(
+                investigation=job.investigation,
+                job_type='nucleus_detection',
+                original_dataset=job.original_dataset,
             )
-            nucleus_detections, nucleus_mask = detect_nuclei(input_image)
-            nucleus_centroids = [
-                (nucleus["Identifier.CentroidX"], nucleus["Identifier.CentroidY"])
-                for nucleus in nucleus_detections
-            ]
-
-        gland_map = skimage.io.imread(io.BytesIO(open(gland_map_path, 'rb').read()))
-        gland_detections, gland_mask = detect_nuclei(gland_map)
-        gland_centroids = [
-            (gland["Identifier.CentroidX"], gland["Identifier.CentroidY"])
-            for gland in gland_detections
+            # Run synchronously
+            run_detect_nuclei(new_job.id, original_dataset_id)
+            resulting_dataset = new_job.resulting_datasets.first()
+        nucleus_detections = list(resulting_dataset.detected_structures.all())
+        nucleus_centroids = [
+            (nucleus.centroid.x, nucleus.centroid.y) for nucleus in nucleus_detections
         ]
 
-        distances = [
-            min([math.dist(nucleus_centroid, gland_centroid) for gland_centroid in gland_centroids])
-            for nucleus_centroid in nucleus_centroids
-        ]
+        # If a previous job has gland detections, use that information
+        # Else run a new job for that information
+        gland_detections = Job.objects.filter(
+            original_dataset=original_dataset,
+            job_type='gland_detection',
+            complete=True,
+        )
+        if gland_detections.count() > 0:
+            resulting_dataset = gland_detections[0].resulting_datasets.first()
+        else:
+            new_job = Job.objects.create(
+                investigation=job.investigation,
+                job_type='gland_detection',
+                original_dataset=job.original_dataset,
+                additional_inputs={'gland_map_path': gland_map_path},
+            )
+            # Run synchronously
+            run_detect_glands(new_job.id, original_dataset_id)
+            resulting_dataset = new_job.resulting_datasets.first()
+        gland_detections = list(resulting_dataset.detected_structures.all())
+        gland_centroids = [(gland.centroid.x, gland.centroid.y) for gland in gland_detections]
+
+        distances = []
+        line_coordinates = []
+        for nucleus_centroid in nucleus_centroids:
+            minimum = None
+            closest = None
+            for gland_centroid in gland_centroids:
+                distance = math.dist(nucleus_centroid, gland_centroid)
+                if not minimum or distance < minimum:
+                    minimum = distance
+                    closest = gland_centroid
+            distances.append(minimum)
+            line_coordinates.append([nucleus_centroid, closest])
         average_distance_to_nearest_gland = sum(distances) / len(distances)
 
         job.resulting_datasets.add(
@@ -76,8 +97,7 @@ def run(job_id: str, original_dataset_id: str, gland_map_path: str):
                 None,
                 {
                     'average_distance_in_pixels': average_distance_to_nearest_gland,
-                    'nucleus_mask': nucleus_mask.tolist(),
-                    'gland_mask': gland_mask.tolist(),
+                    'distance_lines': line_coordinates,
                 },
                 dataset_type='nucleus_gland_dist',
             )
