652ce7394b054bea720e572206be692698b3ec35
Anne Haley <anne.haley@kitware.com>
2022-07-22 08:08:30 -0400

    Remove distance job entirely (can be computed client side)

--- atlascope/core/job_types/__init__.py
+++ atlascope/core/job_types/__init__.py
@@ -1,10 +1,4 @@
-from . import (
-    average_color,
-    brightest_n_pixels,
-    gland_detection,
-    nucleus_detection,
-    nucleus_to_gland_distance,
-)
+from . import average_color, brightest_n_pixels, gland_detection, nucleus_detection
 
 available_job_types = {
     module.__name__.replace(f"{module.__package__}.", ''): module
@@ -13,6 +7,5 @@ available_job_types = {
         brightest_n_pixels,
         gland_detection,
         nucleus_detection,
-        nucleus_to_gland_distance,
     ]
 }
