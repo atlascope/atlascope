4c5d1b57b9b399222de8d5bee0cf6138eab46ccc
Anne Haley <anne.haley@kitware.com>
2022-08-08 10:21:40 -0400

    Improve color selection

--- web/src/utilities/analyticsVis.ts
+++ web/src/utilities/analyticsVis.ts
@@ -1,6 +1,7 @@
 import { DetectedStructure, VisOption } from '@/generatedTypes/AtlascopeTypes';
 import { computed } from '@vue/composition-api';
 import geo from 'geojs';
+import distinctColors from 'distinct-colors';
 import { GeoJSLayer } from './composableTypes';
 import store from '../store';
 import { centroidStringToCoords, NucleusGlandDistance, Point } from './utiltyFunctions';
@@ -17,11 +18,15 @@ const defaultStructureColors = {
   gland: 'red',
 };
 
-const colors: string[] = [];
-while (colors.length < 1000) {
-  const color = Math.floor((Math.random() * 1000000) + 1);
-  colors.push(`#${(`000000${color.toString(16)}`).slice(-6)}`);
-}
+const colors = distinctColors({
+  count: 50,
+  chromaMin: 40,
+  chromaMax: 80,
+  lightMin: 40,
+  lightMax: 80,
+}).map(
+  (color) => color.hex(),
+);
 
 export function visualizeDetectedStructures(
   vis: VisOption,
@@ -47,7 +52,7 @@ export function visualizeDetectedStructures(
         const nearestGland = computedLines.value.find(
           (distance) => distance.nucleus === struct.id,
         )?.gland;
-        customColor = colors[allGlands.indexOf(nearestGland)];
+        customColor = colors[allGlands.indexOf(nearestGland) % colors.length];
       }
       if (vis.options.includes('show_distances_on_hover')) {
         distanceLines = computedLines.value.filter(
